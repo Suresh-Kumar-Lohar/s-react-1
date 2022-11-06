567a5792d8d4311edc0ceeffec79bb5617f12c6f
Suresh-Kumar-Lohar <sureshlohar468@gmail.com>
2022-11-07 00:05:25 +0530

    start task-11-12-using React.create

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,9 +1,11 @@
+import React from 'react'
 import Card from '../UI/Card'
 import ExpenseDate from './ExpenseDate'
 import ExpenseDetails from './ExpenseDetails'
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
+  const date = props.date
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
@@ -14,6 +16,18 @@ const ExpenseItem = (props) => {
       />
     </Card>
   )
+
+  // return React.createElement(
+  //   'div',
+  //   { className: 'expense-item' },
+  //   React.createElement(ExpenseDate, { date: date }),
+  //   React.createElement(
+  //     'div',
+  //     {},
+  //     React.createElement('h1', {}, 'Expense 1'),
+  //     React.createElement('h1', {}, 'Expense 2')
+  //   )
+  // )
 }
 
 export default ExpenseItem
