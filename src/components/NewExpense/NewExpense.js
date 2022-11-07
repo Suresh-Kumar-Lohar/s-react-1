0a6bdd5caeec2a6a362901248bf3b2f556bbdd4b
Suresh-Kumar-Lohar <sureshlohar468@gmail.com>
2022-11-07 23:48:03 +0530

    adv React task-13

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,13 +3,14 @@ import Card from '../UI/Card'
 import './NewExpense.css'
 
 const NewExpense = (props) => {
+  const [isEdit, setIsEdit] = useState(false)
   const [enteredTitle, setEnteredTitle] = useState('')
   const [enteredAmount, setEnteredAmount] = useState('')
   const [enteredDate, setEnteredDate] = useState('')
 
   const submitHandler = (e) => {
     e.preventDefault()
-
+    setIsEdit(false)
     const expenseData = {
       title: enteredTitle,
       amount: enteredAmount,
@@ -24,40 +25,53 @@ const NewExpense = (props) => {
 
   return (
     <Card className='center'>
-      <form onSubmit={submitHandler}>
-        <div>
-          <label>Title</label>
-          <input
-            type='text'
-            value={enteredTitle}
-            onChange={(e) => {
-              setEnteredTitle(e.target.value)
-            }}
-          />
-        </div>
-        <div>
-          <label>Amount</label>
-          <input
-            type='number'
-            value={enteredAmount}
-            min='1'
-            onChange={(e) => {
-              setEnteredAmount(e.target.value)
-            }}
-          />
-        </div>
-        <div>
-          <label>Date</label>
-          <input
-            type='date'
-            value={enteredDate}
-            onChange={(e) => {
-              setEnteredDate(e.target.value)
-            }}
-          />
-        </div>
-        <button>Submit</button>
-      </form>
+      {isEdit ? (
+        <form onSubmit={submitHandler}>
+          <div>
+            <label>Title</label>
+            <input
+              type='text'
+              value={enteredTitle}
+              onChange={(e) => {
+                setEnteredTitle(e.target.value)
+              }}
+            />
+          </div>
+          <div>
+            <label>Amount</label>
+            <input
+              type='number'
+              value={enteredAmount}
+              min='1'
+              onChange={(e) => {
+                setEnteredAmount(e.target.value)
+              }}
+            />
+          </div>
+          <div>
+            <label>Date</label>
+            <input
+              type='date'
+              value={enteredDate}
+              onChange={(e) => {
+                setEnteredDate(e.target.value)
+              }}
+            />
+          </div>
+          <div className='btn'>
+            <button className='button1' type='submit'>
+              Submit
+            </button>
+            <button className='button1' onClick={() => setIsEdit(false)}>
+              Cancel
+            </button>
+          </div>
+        </form>
+      ) : (
+        <button className='button2' onClick={() => setIsEdit(true)}>
+          Add New Expense
+        </button>
+      )}
     </Card>
   )
 }
