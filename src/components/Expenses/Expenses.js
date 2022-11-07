d3ffd5e3b18c329e409c487577e1f669ccdb06fb
Suresh-Kumar-Lohar <sureshlohar468@gmail.com>
2022-11-07 22:47:13 +0530

    adv React task-11

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from '../UI/Card'
 
 import ExpenseItem from './ExpenseItem'
 import './Expenses.css'
+import ExpensesFilter from './ExpensesFilter'
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020')
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear)
+  }
+
+  const filteredExpenses = props.expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear
+  })
   return (
     <Card className='expenses'>
-      {props.expenses.map((item) => (
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {filteredExpenses.map((item) => (
         <ExpenseItem
           key={item.id}
           title={item.title}
